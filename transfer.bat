1648dcfe270e418bba0c125f01cf2213a8c8a38d
Lykakspars <lykakspars@outlook.com>
2018-03-18 13:29:20 +0200

    Documented with REM commands :bulb:

--- transfer.bat
+++ transfer.bat
@@ -1,10 +1,17 @@
 @ECHO OFF
+@REM Getting files by using arguments,
+@REM And setting up.
 SET "Files=%cd%\%1"
 SET /A TmpNumber=(%RANDOM%*100)/998+101
 SET "CUrl=C:\transferwin\curl\bin\curl.exe"
 SET "TempFiles=transfer%TmpNumber%"
+@REM Creating temp file.
 ECHO. >> "%cd%\%TempFiles%"
+@REM Basename-ing Files.
 for /F "delims=" %%i in (%FILE_path%) do SET "basenamed=%%~ni"
+@REM Upload into transfer.sh.
 %CUrl% --progress-bar --upload-file %Files% https://transfer.sh/%basenamed% >> %TempFiles%
+@REM Display URL Results
 TYPE %TempFiles%
+@REM Cleaning up (by removing temp files).
 DEL /F /Q %TempFiles%
